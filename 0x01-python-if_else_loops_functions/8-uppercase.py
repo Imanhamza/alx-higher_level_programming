d7a4785b56f85c0ed8ed85d7710fe809bba3050a
Iman Hamza <emyali697@gmail.com>
2023-03-07 12:07:14 -0800

    8-uppercase.py created

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) < 123:
+            print("{}\n".format(chr(ord(str[i]) - 32)) if i == len(str) - 1
+                    else "{}".format(chr(ord(str[i]) - 32)), end="")
+        else:
+            print("{}\n".format(str[i]) if i == len(str) - 1
+                    else "{}".format(str[i]), end="")
