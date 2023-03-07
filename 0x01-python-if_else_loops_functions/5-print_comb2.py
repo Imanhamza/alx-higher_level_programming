f497842a82b00f58c29c072b6b18340e18858028
Iman Hamza <emyali697@gmail.com>
2023-03-07 10:45:37 -0800

    5-print_comb2.py updated

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print("{}\n".format(i))
-        break;
+        print("{}".format(i))
+        break
     print("{}{},".format(0, i) if i < 10 else "{}, ".format(i), end="")
