5f8dbccfea7812e195319e7536c9dcea225ba793
Iman Hamza <emyali697@gmail.com>
2023-03-07 13:47:49 -0800

    12-fizzbuzz.py updated

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("Fizz ", end="")
         elif i % 5 == 0 and i % 3 != 0:
             if i == 100:
-                print("Buzz")
+                print("Buzz ")
                 break
             print("Buzz ", end="")
         elif i % 15 == 0:
