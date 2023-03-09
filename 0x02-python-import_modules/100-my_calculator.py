455d7b7b778f7f77c12e2a675bc3da8cc535f4ca
Iman Hamza <emyali697@gmail.com>
2023-03-09 12:17:10 -0800

    100-my_calculator.py created

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
+
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == "+":
+        result = add(a, b)
+    elif argv[2] == "-":
+        result = sub(a, b)
+    elif argv[2] == '*':
+        result = mul(a, b)
+    elif argv[2] == "/":
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print("{} {} {} = {}".format(a, argv[2], b, result))
