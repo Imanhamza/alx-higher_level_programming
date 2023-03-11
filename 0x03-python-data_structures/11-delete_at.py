92be36be093ca5ceacee4abc658affc38d994724
Iman Hamza <emyali697@gmail.com>
2023-03-11 07:53:48 -0800

    11-delete_at.py created

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < len(my_list) and idx  > 0:
+        for i in my_list:
+            if i == my_list[idx]:
+                my_list.remove(i)
+        return my_list
+    else:
+        return my_list
