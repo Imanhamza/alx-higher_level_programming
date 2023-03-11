162f87ba1a6f9fd7bde033ac3654b05ea597238d
Iman Hamza <emyali697@gmail.com>
2023-03-11 03:28:14 -0800

    5-no_c.py updated

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string = []
+    new_string = ""
     for i in range(len(my_string)):
         if my_string[i] not in ("c", "C"):
-            new_string.append(my_string[i])
-    return ''.join(new_string)
+            new_string += my_string[i]
+    return new_string
