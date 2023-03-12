8d0dceb993bc269ddbca3c9218b28db74f8bbc52
Iman Hamza <emyali697@gmail.com>
2023-03-12 14:58:54 -0700

    trying another code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,44 +1,54 @@
 #include "lists.h"
 
+
 /**
- * is_palindrome - checks if a singly linked list is a palindrome.
- * @head: A pointer to the head of alinked list
- * Return:0 if it is not a palindrome, 1 if it is a palindrome
+ * add_nodeint - insert a new node at the begining of a list
+ * @head: a pointer to the head of the list
+ * @n: the integer to set for the new element
+ * Return: a pointer to the new node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *node = malloc(sizeof(listint_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->n = (int) n;
+	node->next = *head;
+	*head = node;
+
+	return (node);
+}
+
+/**
+ * is_palindrome - checks if the list is a palindrome
+ * @head: a pointer to the head of the list
+ * Return: 1 if is palindrome 0 if not
  */
 
 int is_palindrome(listint_t **head)
 {
-	/* Handle empty or single node list */
-	if (head == NULL || *head == NULL)
+	listint_t *p = *head;
+	int i, j, count = 0;
+	int arr[5000];
+
+	if (*head == NULL)
 		return (1);
-	listint_t *slow = *head,
-		  *fast = *head;
-	/* Find the middle node */
-	while (fast != NULL && fast->next != NULL)
+
+	while (p)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
+		arr[count] = p->n;
+		p = p->next;
+		count++;
 	}
-	/*reverse the list */
-	listint_t *prev = NULL,
-		  *current = slow,
-		  *next;
-	while (current != NULL)
+	j = count - 1;
+	for (i = 0; i < count / 2; i++, j--)
 	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-	/* compare the first and secod halves */
-	listint_t *node1 = *head,
-		  *node2 = prev;
-	while (node1 != NULL && node2 != NULL)
-	{
-		if (node1->n != node2->n)
+		if (arr[i] != arr[j])
 			return (0);
-		node1 = node1->next;
-		node2 = node2->next;
 	}
+	
 	return (1);
 }
