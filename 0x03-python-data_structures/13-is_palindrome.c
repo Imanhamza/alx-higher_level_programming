ab293de522f65f4819e6f33da646cb040ceca50c
Iman Hamza <emyali697@gmail.com>
2023-03-13 16:10:22 -0700

    update 13-is_palindrome.c with comments and fix betty style

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,7 +8,8 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp = *head;
-	return check_pal(&temp, *head);
+
+	return (check_pal(&temp, *head));
 }
 
 /**
@@ -21,16 +22,19 @@ int check_pal(listint_t **left, listint_t *right)
 {
 	int is_pal;
 
+	/* base case: end of list */
 	if (right == NULL)
 		return (1);
 
+	/* check if the rest of the list is a palindrome */
 	is_pal = check_pal(left, right->next);
 
 	if (is_pal == 0)
 		return (0);
 
+	/* check if the current elements are the same */
 	is_pal = ((*left)->n == right->n);
-
+	/* move left pointer to the next element */
 	*left = (*left)->next;
 
 	return (is_pal);
