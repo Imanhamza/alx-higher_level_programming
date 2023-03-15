c689ddb277fdba30446e2e4e385f5dd80202441a
Iman Hamza <emyali697@gmail.com>
2023-03-15 13:02:48 -0700

    100-weight_average.py created

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        top = 0
+        bottom = 0
+        for i in my_list:
+            mul = 1
+            for j in range(len(i)):
+                mul *= i[j]
+                if j == 1:
+                    bottom += i[j]
+            top += mul
+        return top / bottom
