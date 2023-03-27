b2e1fa16cd1537211fb59c65ff59c358ccef404a
Iman Hamza <emyali697@gmail.com>
2023-03-27 14:41:00 -0700

    adding sys module and update error category

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
         return True
-    except Exception as err:
+    except (TypeError, ValueError) as err:
         sys.stderr.write("Exception: {}".format(err))
         return False
