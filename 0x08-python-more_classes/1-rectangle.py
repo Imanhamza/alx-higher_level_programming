d66356459297ef21eb9eaeed99e862aa5454bbf9
Iman Hamza <emyali697@gmail.com>
2023-04-03 10:54:31 -0700

    update the initionation of the width and height values

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
         height (int): Private instance attribute
         '''
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     # Start setting width
 
