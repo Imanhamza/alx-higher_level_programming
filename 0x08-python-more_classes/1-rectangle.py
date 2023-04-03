071ff13a59b4728eb3bd14edc3b8af463cc92841
Iman Hamza <emyali697@gmail.com>
2023-04-03 10:18:03 -0700

    1-rectangle.py created

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+'''
+A class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
+'''
+
+
+class Rectangle:
+    ''' Class to compute rectangle equations '''
+
+    def __init__(self, width=0, height=0):
+        ''' initiation of a calss has args
+        width (int): Private instance attribute
+        height (int): Private instance attribute
+        '''
+
+        self.__width = width
+        self.__height = height
+
+    # Start setting width
+
+    @property
+    def width(self):
+        ''' A function to retrive the width '''
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        ''' A function to set the value of the width '''
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+    # Start setting height
+
+    @property
+    def height(self):
+        ''' A function to retrive the height '''
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        ''' A function to set the height '''
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
