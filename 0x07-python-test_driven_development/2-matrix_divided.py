697090fbbc556332ed11a1204f33f593168fb447
Iman Hamza <emyali697@gmail.com>
2023-04-05 08:35:50 -0700

    handle empty list

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,16 +4,21 @@ def matrix_divided(matrix, div):
 
     if div == 0:
         raise ZeroDivisionError('division by zero')
-    elif type(div) is not int and type(div) is not float:
+    if type(div) is not int and type(div) is not float:
         raise TypeError('div must be a number')
-
+    if type(matrix) is not list or (len(matrix) == 0) or type(matrix[0]) is not list or (len(matrix[0]) == 0):
+        raise TypeError('Each row of the matrix must have the same size')
+    
     new_matrix =[]
     for i in matrix:
-        if (len(i) != len(matrix[0])):
+        if type(i) is not list:
+            raise TypeError('Each row of the matrix must have the same size')
+        if len(i) != len(matrix[0]):
             raise TypeError('Each row of the matrix must have the same size')
 
         for j in i:
             if type(j) is not int and type(j) is not float:
                 raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-        new_matrix = [list(map(lambda x: round(x / div, 2), row)) for row in matrix]
+    
+    new_matrix = [list(map(lambda x: round(x / div, 2), row)) for row in matrix]
     return new_matrix
