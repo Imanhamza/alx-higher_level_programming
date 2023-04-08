1e1ba45d49f903d7820485e028766aeed3ac8ade
Iman Hamza <emyali697@gmail.com>
2023-04-07 17:42:59 -0700

    retrive the last version

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -11,10 +11,8 @@ def print_square(size):
 
     if not isinstance(size, int):
         raise TypeError('size must be an integer')
-    elif size < 0:
+    if size < 0:
         raise ValueError('size must be >= 0')
-    if size == 0:
-        return
     for i in range(size):
         for j in range(size):
             print('#', end='')
