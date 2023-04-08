934feaf7cf94c38369786ad4b71ff98a45df06e7
Iman Hamza <emyali697@gmail.com>
2023-04-07 17:41:52 -0700

    handle size 0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -13,7 +13,8 @@ def print_square(size):
         raise TypeError('size must be an integer')
     elif size < 0:
         raise ValueError('size must be >= 0')
-
+    if size == 0:
+        return
     for i in range(size):
         for j in range(size):
             print('#', end='')
