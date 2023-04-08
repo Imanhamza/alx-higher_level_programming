1c15119c09dce58acc4ee5f5db3cfe3752660fa2
Iman Hamza <emyali697@gmail.com>
2023-04-07 17:30:09 -0700

    tests/4-print_square.txt and 4-print_square.py created

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' A module to print a square '''
+
+
+def print_square(size):
+    '''
+    A function to print out a '#' square
+    args:
+    size (int): the length of the faces
+    '''
+
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+        print()
