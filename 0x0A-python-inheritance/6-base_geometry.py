4716828defb327fe94f54ea5d0099c18e8432b22
Iman Hamza <emyali697@gmail.com>
2023-04-11 11:35:35 -0700

    6-base_geometry.py created

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' A class BaseGeometry (based on 5-base_geometry.py). '''
+
+
+class BaseGeometry:
+    ''' A class '''
+
+    def area(self):
+        ''' function to compute area '''
+
+        raise Exception("area() is not implemented")
