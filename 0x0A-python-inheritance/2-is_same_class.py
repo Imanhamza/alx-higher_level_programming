fda2151710031b664ea07897813a43462a0fa0f7
Iman Hamza <emyali697@gmail.com>
2023-04-11 10:48:58 -0700

    2-is_same_class.py created

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' A module to check if the object is instance or not '''
+
+
+def is_same_class(obj, a_class):
+    ''' A function to return True or Flase for the instance '''
+
+    return isinstance(obj, a_class)
