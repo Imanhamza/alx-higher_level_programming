f470110bb596f3bfcfc5a7d3809621700609b691
Iman Hamza <emyali697@gmail.com>
2023-04-11 11:19:17 -0700

    3-is_kind_of_class.py created

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' A module to check the inheritance of object '''
+
+
+def is_kind_of_class(obj, a_class):
+    ''' A function to return True or False '''
+
+    return isinstance(obj, a_class)
