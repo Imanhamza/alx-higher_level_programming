5c8cd6eab68fe2fcd2f55a4a3fb11390fdb6917b
Iman Hamza <emyali697@gmail.com>
2023-04-12 15:12:01 -0700

    11-student.py created

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' A class Student that defines a student by: (based on 10-student.py) '''
+
+
+class Student:
+    ''' A class Student with args:
+    first_name(str): Public instance attribute
+    last_name(str): Public instance attribute
+    age(int): Public instance attribute
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        ''' initiation of the class '''
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        ''' A function returns a dictionary of decription '''
+
+        if type(attr) == list:
+            dic = {i: self.__dict__[i] for i in self.__dict__.keys() & attr}
+            return dic
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        ''' A function replaces all attributes of the Student instance '''
+
+        for i, j in json.items():
+            self.__dict__[i] = j
