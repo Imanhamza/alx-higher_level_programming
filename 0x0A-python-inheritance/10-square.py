0938b2226864e56ce082db160ae691ae829d5d0c
Iman Hamza <emyali697@gmail.com>
2023-04-11 17:31:31 -0700

    edit syntax mistake

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,7 +5,7 @@ A class Square that inherits from Rectangle (9-rectangle.py).
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(.Rectangle):
+class Square(Rectangle):
     ''' A class deals with a square '''
 
     def __init__(self, size):
