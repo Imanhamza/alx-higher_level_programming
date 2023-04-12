3b60c76b23be8908a7bf403304a76a3b4294a49c
Iman Hamza <emyali697@gmail.com>
2023-04-11 17:48:17 -0700

    100-my_int.py created

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' A class MyInt that inherits from int '''
+
+
+class MyInt(int):
+    ''' A class deals with int '''
+
+    def _myint(self):
+        self.__int = my_int
+        return my_int
