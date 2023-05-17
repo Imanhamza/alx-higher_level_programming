29a0309e6c6c9e935d38038ada3514e254d237ff
Iman Hamza <iman.ali.hamza@gmail.com>
2023-05-17 16:24:26 -0700

    update 102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- A script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-select ts.title, SUM(r.rate) AS rating
+SELECT ts.title, SUM(r.rate) AS rating
 FROM tv_shows AS ts
 JOIN tv_show_ratings AS r
 ON ts.id = r.show_id
