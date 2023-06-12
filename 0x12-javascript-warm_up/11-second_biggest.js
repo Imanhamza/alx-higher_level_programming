7b78c505af0b7d2d63f5508eefb85974b54dfdef
Iman Hamza <iman.ali.hamza@gmail.com>
2023-06-12 13:11:54 -0700

    update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,12 +5,13 @@ const argu = process.argv;
 
 if (argu.length === 3 || argu.length < 3) {
   console.log(0);
-} else if (argu.length > 3) {
+} else {
   const num = [];
   for (let i = 2; i < argu.length; i++) {
     num.push(parseInt(argu[i]));
   }
   const index = num.length - 1;
   const sortedNum = num.sort();
+  console.log(sortedNum);
   console.log(sortedNum[index - 1]);
 }
