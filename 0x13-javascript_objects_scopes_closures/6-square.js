285f2bb4ffadc423ea9ae3ca052d1c8233519271
Iman Hamza <iman.ali.hamza@gmail.com>
2023-06-13 16:35:17 -0700

    create 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.width; i++) {
+      let line = '';
+      for (let j = 0; j < this.height; j++) {
+        if (c) {
+          line += c;
+        } else {
+          line += 'X';
+        }
+      }
+      console.log(line);
+    }
+  }
+}
+module.exports = Square;
