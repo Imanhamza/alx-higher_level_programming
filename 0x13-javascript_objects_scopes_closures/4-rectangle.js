cf2c0b6d347ca1ae91ed8949c1165bc252ebf50e
Iman Hamza <iman.ali.hamza@gmail.com>
2023-06-13 16:02:16 -0700

    create 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    this.width = this.height;
+    this.height = this.width;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.width * 2;
+  }
+}
+module.exports = Rectangle;
