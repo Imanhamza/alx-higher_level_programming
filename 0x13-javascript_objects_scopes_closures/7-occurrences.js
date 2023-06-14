70dccc54087dab506bbe86467e3968dcfefe546a
Iman Hamza <iman.ali.hamza@gmail.com>
2023-06-13 17:01:48 -0700

    create 7-occurrences.js

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (const element in list) {
+    if (list[element] === searchElement) {
+      counter += 1;
+    }
+  }
+  return (counter);
+};
