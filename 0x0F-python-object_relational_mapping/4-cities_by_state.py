434ee91033d4e55804875bbc4ab94dae690b21cd
Iman Hamza <iman.ali.hamza@gmail.com>
2023-06-16 17:36:42 -0700

    create 4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' A script that lists all cities from the database hbtn_0e_4_usa'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    # connect the database
+    db = MySQLdb.connect(
+            user=argv[1], passwd=argv[2],
+            database=argv[3], host="localhost", port=3306)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # create the query and execute
+    query = '''SELECT c.id, c.name, s.name
+    FROM states AS s
+    LEFT JOIN cities AS c
+    ON s.id = c.state_id
+    ORDER BY c.id'''
+    cursor.execute(query)
+
+    # fetch all rows in the result and print them all
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
