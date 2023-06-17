611af0484f204c7b2a8b3c4ad0df42aa46798b74
Iman Hamza <iman.ali.hamza@gmail.com>
2023-06-16 17:39:17 -0700

    edit join status

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     # create the query and execute
     query = '''SELECT c.id, c.name, s.name
     FROM states AS s
-    LEFT JOIN cities AS c
+    JOIN cities AS c
     ON s.id = c.state_id
     ORDER BY c.id'''
     cursor.execute(query)
