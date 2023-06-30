50f12b49a047e26f43257ade5069625f3bca1a4d
Iman Hamza <iman.ali.hamza@gmail.com>
2023-06-30 16:21:45 -0700

    create 6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' a function that finds a peak in a list of unsorted integers '''
+
+
+def find_peak(list_of_integers):
+    # left and right pointrs
+    left = 0
+    right = len(list_of_integers) - 1
+
+    if len(list_of_integers) == 0:
+        return None
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+    return list_of_integers[left]
