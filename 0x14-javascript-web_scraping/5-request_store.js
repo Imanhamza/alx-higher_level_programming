cbfc301b84b7664e910158013d1dccd5ae9f49cd
Iman Hamza <iman.ali.hamza@gmail.com>
2023-07-25 23:53:05 +0000

    create 5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// A script that gets the contents of a webpage and stores it in a file.
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get({ url, encoding: null }, (error, response, body) => {
+ if (error) {
+  console.log(error);
+ } else {
+  const content = body.toString('utf-8');
+  fs.writeFile(filePath, content, 'utf-8', (err) => {
+if (err) {
+console.log(err);
+} else {
+  console.log(`Webpage content saved to file: ${filePath}`);
+}
+});
+}
+});
