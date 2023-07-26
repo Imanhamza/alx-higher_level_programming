1ab64ec735a52f9c96a05a7fb9647cc90221e99d
Iman Hamza <iman.ali.hamza@gmail.com>
2023-07-26 00:01:43 +0000

    update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,17 +6,4 @@ const request = require('request');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request.get({ url, encoding: null }, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = body.toString('utf-8');
-    fs.writeFile(filePath, content, 'utf-8', (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(`Webpage content saved to file: ${filePath}`);
-      }
-    });
-  }
-});
+request(url).pipe(fs.createWriteStream(filePath));
